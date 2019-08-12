870720a55bb36cd3dd49b876477961fd2c945cac
Will Schulz <will.schulz29@gmail.com>
2019-08-11 21:52:12 -0400

    link change project 2

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -119,3 +119,36 @@ const Contact = () => {
 }
 
 export default Contact;
+
+import React from 'react';
+
+const withSecretToLife = (WrappedComponent) => {
+  class HOC extends React.Component {
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          secretToLife={42}
+        />
+      );
+    }
+  }
+    
+  return HOC;
+};
+
+export default withSecretToLife;
+
+
+import React from 'react';
+import withSecretToLife from 'components/withSecretToLife';
+
+const DisplayTheSecret = props => (
+  <div>
+    The secret to life is {props.secretToLife}.
+  </div>
+);
+
+const WrappedComponent = withSecretToLife(DisplayTheSecret);
+
+export default WrappedComponent;
--- src/components/projects/Project2.js
+++ src/components/projects/Project2.js
@@ -1,45 +1,58 @@
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-import './projects.css'
+import "./projects.css";
 
-const calc = (x, y) => [(y - window.innerHeight / 2) / 200, -(x - window.innerWidth / 2) / 200, 1.1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+const calc = (x, y) => [
+  (y - window.innerHeight / 2) / 200,
+  -(x - window.innerWidth / 2) / 200,
+  1.1
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 function Project2() {
   const [props, set] = useSpring(() => ({
-    xys: [0, 0, .75],
-    config: { mass: 5, tension: 350, friction: 40 },
-  }))
+    xys: [0, 0, 0.75],
+    config: { mass: 5, tension: 350, friction: 40 }
+  }));
   return (
     <>
-    <animated.div
-      className="project-card pc2"
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, .75] })}
-      style={{ transform: props.xys.interpolate(trans) }}
-    >
-      <a href="https://github.com/kittenman29/Back-End-Trip-Split">
-        <FontAwesomeIcon className="icon-one fa-3x" icon={faGithub} />
-      </a>
-    </animated.div>
-    <div classname="mobile-project-card">
-      <div className="mc1">
-      <div className="mobile-project-header-container">
-        <h3 className="mobile-card-header">Back End Project</h3>
-        <a className="mobile-card-link" href="https://github.com/kittenman29/Back-End-Trip-Split">
-          <FontAwesomeIcon className="mobile-icon-one fa-3x" icon={faGithub} />
+      <animated.div
+        className="project-card pc2"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 0.75] })}
+        style={{ transform: props.xys.interpolate(trans) }}
+      >
+        <a href="https://github.com/trip-split/Back-End-Trip-Split">
+          <FontAwesomeIcon className="icon-one fa-3x" icon={faGithub} />
         </a>
-      </div>
-        <div className="mobile-card-body">
-          <p classname="mobile-card">Node, Express, PostgreSql, bcryptjs, jwt</p>
+      </animated.div>
+      <div classname="mobile-project-card">
+        <div className="mc1">
+          <div className="mobile-project-header-container">
+            <h3 className="mobile-card-header">Back End Project</h3>
+            <a
+              className="mobile-card-link"
+              href="https://github.com/trip-split/Back-End-Trip-Split"
+            >
+              <FontAwesomeIcon
+                className="mobile-icon-one fa-3x"
+                icon={faGithub}
+              />
+            </a>
+          </div>
+          <div className="mobile-card-body">
+            <p classname="mobile-card">
+              Node, Express, PostgreSql, bcryptjs, jwt
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
 
 export default Project2;
